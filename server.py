249fa571c20ec2658c748eb71559b8dedaefd192
rotemlipo <rotemlipo@gmail.com>
2016-03-19 12:56:56 +0200

    fixed bug in register function (saving list of users instead of saving one user)

--- server.py
+++ server.py
@@ -5,7 +5,8 @@ import Group
 import UserReciever
 import User
 
-
+usersFileName = "D:/newNewnewserverfinalversionnewNewnew2/users.pkl"
+groupsFileName = "D:/newNewnewserverfinalversionnewNewnew2/groups.pkl"
 onlineUsers = [] ##list of the users that are currently connected
 messages = []
 count = 0
@@ -52,7 +53,7 @@ class Connection(Thread):
                 self.NewGroup(l)
 
     def FindingUserGroups(self,clientAddr):
-        groupsfile = open("D:/newNewnewserverfinalversionnewNewnew2/groups.pkl","rb")
+        groupsfile = open(groupsFileName,"rb")
         usergroups = []
         l1 = pickle.load(groupsfile)
         groupsfile.close()
@@ -69,13 +70,13 @@ class Connection(Thread):
             for user1 in onlineUsers:
                 if user1[0].getUserName() == username:
                     Group.AddUser(user1)
-        groupsfile = open("D:/newNewnewserverfinalversionnewNewnew2/groups.pkl","rb")
+        groupsfile = open(groupsFileName,"rb")
         pickle.dump(gr)
         groupsfile.close()
 
 
     def CheckLogin(self, userName,password):
-        userfile = open("D:/newNewnewserverfinalversionnewNewnew2/users.pkl", "rb")
+        userfile = open(usersFileName, "rb")
         l = pickle.load(userfile)
         userfile.close()
         for u in l:
@@ -83,18 +84,38 @@ class Connection(Thread):
                 return u
         return 0
 
+
     def Register(self,userName , password, firstName, lastName):
-        userfile = open("D:/newNewnewserverfinalversionnewNewnew2/users.pkl", "rb")
+        """
+        the function checks if the user's username already exists and if it doesnt it creates the new user
+        :param userName:
+        :param password:
+        :param firstName:
+        :param lastName:
+        :return: user (if the username didnt exist), 0 (if the username exist)
+        """
         user = User.User(firstName, lastName, password,userName)
-        l = pickle.load(userfile)
-        for u in l:
-            if u.getusername() == userName:
-                return 0
-        userfile.close()
-        userfile=open("D:/newNewnewserverfinalversionnewNewnew2/users.pkl","w+")
-        pickle.dump(user,userfile)
-        userfile.close()
+        listOfUsers = pickle.load(open(usersFileName, "rb"))
+        print listOfUsers
+        for user1 in listOfUsers:
+            if user1.GetUserName() == userName:
+                return 0 #the username exists
+        self.SaveNewUser(user,listOfUsers)
         return user
+
+
+
+
+    def SaveNewUser(self, user, oldList):
+        """
+        the username does not exist, we add the new user to the list in the pickle
+        :param user:
+        :param oldList:
+        """
+        oldList.append(user)
+        pickle.dump(oldList,open(usersFileName,"wb"))
+
+
 
 
 c=Connection()
